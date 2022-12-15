f5c5e4f42314b767ca2e53290ab1d8aab0054b37
junhyuk <wwns1009.gmail.com>
2022-12-15 10:53:15 +0900

    spa

---
+++ routes/carts.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const Goods = require("../schemas/goods");
+const Cart = require("../schemas/cart");
+const router = express.Router();
+
+router.get("/carts", async (req, res) => {
+  const carts = await Cart.find();
+  const goodsIds = carts.map((cart) => cart.goodsId);
+
+  const goods = await Goods.find({ goodsId: goodsIds });
+ // goods에 해당하는 모든 정보를 가지고 올건데
+ // 만약 goodsIds 변수안에 존재하는 값일 떄에만 조회하라
+  const results = carts.map((cart) => {
+		return {
+			quantity: cart.quantity,
+			goods: goods.find((item) => item.goodsId === cart.goodsId)
+		};
+  });
+
+  res.json({
+    carts: results,
+  });
+});
+
+module.exports = router;
